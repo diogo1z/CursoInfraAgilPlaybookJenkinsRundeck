89ec50b52800dd721a81602235a56dfff6e7fe4f
Diogo Matias <diogo1z@hotmail.com>
2018-04-20 22:50:20 +0000

    Subindo para github

---
+++ rundeck.pp
@@ -0,0 +1,17 @@
+
+# rodar puppet apply -f rundeck.pp
+
+package {"java-1.8.0":
+   ensure=>present
+ }
+
+ package{"http://repo.rundeck.org/latest.rpm":
+   ensure => present,
+ }
+
+ package{"rundeck":
+   ensure => present,
+ }
+ service{"rundeckd":
+   ensure => running,
+ }
